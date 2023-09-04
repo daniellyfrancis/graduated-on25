a0c45a4905b298a4906c63581ee2e787b93aa953
daniellyfrancis <francisdanielly04@gmail.com>
2023-09-04 02:59:26 +0100

    data testing

---
+++ graduated-on25/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <h6>© 2023. Desenvolvido com amor por Danielly Francis 💖. Todos os direitos reservados.</h6>
+    </footer>
+  )
+};
+
+export default Footer;
