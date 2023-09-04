5b9e68ee8cf6cdb181c15a9d7cf9f95276c58937
daniellyfrancis <francisdanielly04@gmail.com>
2023-09-04 01:42:55 +0100

    graduatesData JSON initial

---
+++ graduated-on25/src/graduatesData.js
@@ -0,0 +1,19 @@
+const graduatesData = [
+  {
+    name: 'Danielly Francis',
+    id: 1,
+    image: 'https://avatars.githubusercontent.com/u/65682057?s=96&v=4',
+    area: 'Frontend',
+    linkedin: 'https://www.linkedin.com/in/daniellyfrancisnascimento/',
+    github: 'https://github.com/daniellyfrancis'
+  },
+  {
+    name: 'fulana de tal',
+    id: 1,
+    image: 'https://www.gala.fr/imgre/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2Fgal.2Fvar.2Fgal.2Fstorage.2Fimages.2Fmedia.2Fmultiupload_du_07_decembre_2016.2Fbeyonce.2F3931304-1-fre-FR.2Fbeyonce.2Ejpg/1504x846/quality/80/beyonce-devient-l-artiste-feminine-la-plus-nommee-de-l-histoire-des-grammy-awards.jpg',
+    linkedin: 'https://www.gala.fr/l_actu/news_de_stars/beyonce_devient_l_artiste_feminine_la_plus_nommee_de_l_histoire_des_grammy_awards_380566',
+    github: 'https://www.google.com'
+  },
+];
+
+export default graduatesData;
