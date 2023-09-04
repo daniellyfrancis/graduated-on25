656b7d64c824952ad08d0efce8e0b636fe266144
daniellyfrancis <francisdanielly04@gmail.com>
2023-09-04 02:58:53 +0100

    header component

---
+++ graduated-on25/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>On25 Graduates</h1>
+      <p>Aqui você pode encontrar o perfil de todas as alunas formadas na turma On25 do curso Imersão Javascript da Reprograma.</p>
+      <div>more informations</div>
+    </header>
+  );
+};
+
+export default Header;
