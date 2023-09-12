9b056c1e5bfa555054a43da23cebb4f94c620dac
daniellyfrancis <francisdanielly04@gmail.com>
2023-09-12 23:08:25 +0100

    developer updates

--- src/components/DeveloperCard.js
+++ src/components/DeveloperCard.js
@@ -6,27 +6,27 @@ import linkedinImage from '../assets/img/linkedin.png';
 
 const DeveloperCard = ({ developer }) => {
   return (
-    <div className="developer-card">
-      <div>
-        <Tilt>
-        <img className="img-profile" src={developer.image} alt={developer.name} />
-        </Tilt>
-          <h2>{developer.name}</h2>
-          <h5>{developer.area}</h5>
-          <div className="social-media">
-            <div className='github-image'>
-              <a href={developer.github} target="_blank">
-                <img src={githubImage} />
-              </a>
-            </div>
-            <div className='linkedin-image'>
-              <a href={developer.linkedin} target="_blank">
-                <img src={linkedinImage} />
-              </a>
-            </div>
+    <Tilt>
+      <div className="developer-card">
+        <div className="developer-icon">
+          <img src={developer.image} alt={developer.name} />
+        </div>
+        <h2>{developer.name}</h2>
+        <h5>{developer.area}</h5>
+        <div className="social-media">
+          <div className='github-image'>
+            <a href={developer.github} target="_blank">
+              <img src={githubImage} />
+            </a>
           </div>
+          <div className='linkedin-image'>
+            <a href={developer.linkedin} target="_blank">
+              <img src={linkedinImage} />
+            </a>
+          </div>
+        </div>
       </div>
-    </div>
+    </Tilt>
   );
 };
 
