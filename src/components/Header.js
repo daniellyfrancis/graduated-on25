4f4fd85e7ae80691dadc3b64864d4075aaa48c9f
daniellyfrancis <francisdanielly04@gmail.com>
2023-09-12 23:07:48 +0100

    header updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <header>
       <h1>On25 Graduates</h1>
-      <p>Aqui você pode encontrar o perfil de todas as alunas formadas na turma On25 do curso Imersão Javascript da Reprograma.</p>
+      <p>Aqui você pode encontrar o perfil das alunas formadas na turma On25<br /> do curso Imersão Javascript da Reprograma.</p>
       <div className="reprograma-logo">
         <a href="https://reprograma.com.br/curso-imersao-javascript/" target="_blank">
           <img src={reprogramaLogo}/>
